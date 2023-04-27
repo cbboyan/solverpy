81d153a81bc1bff1b1e37a267e33298c21f94508
Jan Jakubuv <jakubuv@gmail.com>
2023-04-27 03:32:15 +0200

    default plugins

---
+++ src/solverpy/solver/plugins/__init__.py
@@ -0,0 +1,33 @@
+from .db.bid import Bid
+from .db.errors import Errors
+from .db.jsons import Jsons
+from .db.outputs import Outputs
+from .db.sid import Sid
+from .db.solved import Solved
+from .db.status import Status
+from .db.yamls import Yamls
+
+def dbfiles():
+   return [
+      Bid(),
+      Sid(),
+      Jsons(),
+      Solved(),
+      Errors(),
+      Status(),
+   ]
+
+def dbdebug():
+   return [
+      Bid(),
+      Sid(),
+      Jsons(),
+      Yamls(),
+      Solved(),
+      Outputs(),
+      Errors(),
+      Status(),
+   ]
+
+__all__ = [dbfiles, dbdebug]
+
