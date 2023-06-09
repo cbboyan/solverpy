1543126cc3f0728180d38b6d1d03aeccf5b813f5
Jan Jakubuv <jakubuv@gmail.com>
2023-06-10 00:30:20 +0200

    default memory limit to 2GB

--- src/solverpy/solver/plugins/shell/memory.py
+++ src/solverpy/solver/plugins/shell/memory.py
@@ -4,8 +4,8 @@ ULIMIT_CMD = "ulimit -Sv %d"
 
 class Memory(Decorator):
    
-   def __init__(self, giga=1):
-      self.prefix = ULIMIT_CMD % 10 # % int(giga * 1000000)
+   def __init__(self, giga=2):
+      self.prefix = ULIMIT_CMD % int(giga * 1000000)
    
    def decorate(self, cmd):
       return f"{self.prefix} && {cmd}"
