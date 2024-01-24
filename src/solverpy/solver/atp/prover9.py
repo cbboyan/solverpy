8550265d40e5f7d13784b04a7b930968ef5f8417
Jan Jakubuv <jakubuv@gmail.com>
2024-01-24 14:08:54 +0100

    fix: setup for a general solver

--- src/solverpy/solver/atp/prover9.py
+++ src/solverpy/solver/atp/prover9.py
@@ -1,7 +1,9 @@
 import re
 
 from ..stdinsolver import StdinSolver
+from ..tptpsolver import TPTP_OK, INC_OK, TPTP_ALL, TPTP_TIMEOUT
 from ...tools import patterns, human
+from ..plugins.shell.time import Time
 
 P9_BINARY = "prover9"
 
@@ -25,11 +27,7 @@ P9_STATS = re.compile(r"^=+ STATISTICS =+$(.*)^=+ end of statistics =+$",
 # pattern to extract a single statistic (key/val pair)
 P9_SINGLE = re.compile(r"(\w*)=([0-9.]*[0-9])")
 
-P9_PROVED = re.compile(r"^THEOREM PROVED$", flags=re.MULTILINE)
-
-P9_FAILED = re.compile(r"^SEARCH FAILED$", flags=re.MULTILINE)
-
-# TPTP compatible statuses for Prover9 termination reasons
+# TPTP-compatible statuses for Prover9 termination reasons
 P9_STATUS = {
    "max_seconds": "Timeout",
    "max_megs": "ResourceOut",
@@ -37,33 +35,14 @@ P9_STATUS = {
    "sos_empty": "Satisfiable",
 }
 
-TPTP_OK = frozenset([
-   'Satisfiable', 
-   'Unsatisfiable', 
-   'Theorem', 
-   'CounterSatisfiable', 
-   'ContradictoryAxioms',
-])
-
-TPTP_TIMEOUT = frozenset([
-   'ResourceOut', 
-   'Timeout',
-   "TIMEOUT", # simulated timeout
-])
-
 class Prover9(StdinSolver):
 
    def __init__(self, limit, binary=P9_BINARY, static=P9_STATIC, complete=True, plugins=[]):
+      plugins = plugins + [ Time() ] 
       StdinSolver.__init__(self, binary, limit, P9_BUILDER, plugins, None, static)
+      self.complete = complete
 
    def process(self, output):
-      #if P9_PROVED.search(output):
-      #   status = "Theorem"
-      #elif P9_FAILED.search(output):
-      #   status = None # use `reason` as status later
-      #else:
-      #   return dict(status="ERROR")
-
       reason = P9_REASON.search(output)
       if not reason:
          return {}
@@ -79,11 +58,18 @@ class Prover9(StdinSolver):
       result["status"] = P9_STATUS[reason] if (reason in P9_STATUS) else reason
 
       result = patterns.mapval(result, human.numeric)
+
+      if "User_CPU" in result:
+         result["runtime"] = result["User_CPU"]
+
       return result
+
+   def valid(self, result):
+      return super().valid(result) and result["status"] in TPTP_ALL
 
    @property
    def success(self):
-      return TPTP_OK 
+      return TPTP_OK if self.complete else INC_OK
 
    @property
    def timeouts(self):
