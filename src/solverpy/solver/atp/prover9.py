f3b6c92bbc79868d3760aff59fc000f3bb9634b9
Jan Jakubuv <jakubuv@gmail.com>
2024-01-27 10:11:07 +0100

    fix: ensure prover9 terminates

--- src/solverpy/solver/atp/prover9.py
+++ src/solverpy/solver/atp/prover9.py
@@ -40,7 +40,7 @@ class Prover9(StdinSolver):
 
    def __init__(self, limit, binary=P9_BINARY, static=P9_STATIC, complete=True, plugins=[]):
       plugins = plugins + [ Time() ] 
-      StdinSolver.__init__(self, binary, limit, P9_BUILDER, plugins, None, static)
+      StdinSolver.__init__(self, binary, limit, P9_BUILDER, plugins, 2, static)
       self.complete = complete
 
    def process(self, output):
