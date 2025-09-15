c6ff940c32dd2096dfa9b2cc820d26e416ac8675
Jan Jakubuv <jakubuv@gmail.com>
2025-09-16 01:24:39 +0200

    Deployed ed7a823 with MkDocs version: 1.6.1

--- js/solverpy.js
+++ js/solverpy.js
@@ -1,8 +1,23 @@
-document.querySelector('.clickable-image').addEventListener('click', function() {
-  const container = this.parentElement;
-  if (container.classList.contains('fullscreen')) {
-    container.classList.remove('fullscreen');
-  } else {
-    container.classList.add('fullscreen');
-  }
+//
+//document.querySelector('.clickable-image').addEventListener('click', function() {
+//  const container = this.parentElement;
+//  if (container.classList.contains('fullscreen')) {
+//    container.classList.remove('fullscreen');
+//  } else {
+//    container.classList.add('fullscreen');
+//  }
+//});
+
+const images = document.querySelectorAll('.clickable-image');
+
+images.forEach(function(image) {
+  image.addEventListener('click', function() {
+    const container = this.parentElement;
+    if (container.classList.contains('fullscreen')) {
+      container.classList.remove('fullscreen');
+    } else {
+      container.classList.add('fullscreen');
+    }
+  });
 });
+
