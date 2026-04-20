e5d332bbf011825a979e2bf419dab664c167ec31
Jan Jakubuv <jakubuv@gmail.com>
2026-04-20 17:13:12 +0200

    chore: updated grackle ramparils wrapper script

--- scripts/grackle/grackle-ramparils.py
+++ scripts/grackle/grackle-ramparils.py
@@ -13,17 +13,16 @@ if __name__ == '__main__':
       conf["timeout"] = cutoff
       runner = load_class(conf["cls"])(config=conf)
       inst = sys.argv[2]
+      runner.config["cutoff"] = int(float(sys.argv[3]))
 
       params = runner.parse(sys.argv[4:])
       fixed = conf.get("fixed", {})
       params = runner.clean(params)
       params.update(fixed)
       res = runner.run(params, inst)
-      if not res:
-         res = [9999999999, 9999999999]
-      (quality, clock) = res[:2]
-
-      print(RESULT % ("OK", clock, quality))
+      if res:
+         (quality, clock, status) = res[:3]
+         print(RESULT % (status, clock, quality))
 
    if len(sys.argv) < 4:
       print("usage: %s runner_config instance cutoff_time -param1 val1 ..." % sys.argv[0])
